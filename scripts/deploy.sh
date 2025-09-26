f2918b9d3af992e3929a892ffce08adab051b3e5
vipul-space23 <vipulpatil2500@gmail.com>
2025-09-26 13:34:59 +0530

    Fix execute permissions

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# This is a standard first line for shell scripts
+
+echo "Build finished, running deployment script..."
+# Add any other commands you need here
+echo "Deployment script finished successfully!"
